d72d6aadee5e61028c6351186e4ec1ae4f57f00f
Weny <782874135@qq.com>
2020-09-02 16:36:04 +0800

    屏蔽

--- springcloud-service/src/main/java/com/github/weny/springcloud/springcloudservice/controller/SpringcloudServiceControl.java
+++ springcloud-service/src/main/java/com/github/weny/springcloud/springcloudservice/controller/SpringcloudServiceControl.java
@@ -1,19 +1,16 @@
 package com.github.weny.springcloud.springcloudservice.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.github.weny.springcloud.springcloudcommon.entity.Student;
 import com.github.weny.springcloud.springcloudcommon.model.StudentDTO;
-import com.github.weny.springcloud.springcloudservice.service.StudentService;
-import io.seata.spring.annotation.GlobalTransactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
 
 /**
  * @author: Weny
@@ -27,8 +24,8 @@ public class SpringcloudServiceControl {
 
     @Resource
     private RedisTemplate redisTemplate;
-    @Autowired
-    private StudentService studentService;
+//    @Autowired
+//    private StudentService studentService;
 
     @GetMapping("/getstudent")
     public StudentDTO getStudent(String name) {
@@ -87,29 +84,29 @@ public class SpringcloudServiceControl {
      * @desc 分布式事务seata，双数据源学习 --https://blog.csdn.net/qq_28193409/article/details/89537216
      */
 //    @GetMapping("/addStu")
-    @GlobalTransactional
-    public String galobTrans(int age,String name){
-            //db2+1 简单期间2班新来一个同学
-            Student  student = new Student();
-            student.setAge(age);
-            student.setName(name);
-            student.setCode(UUID.randomUUID().toString());
-            Student student1 = studentService.insert2(student);
-            //db1- 1班随机kill一个小朋友
-//            List<Student> students = studentService.queryAll();
-//            int number = new Random().nextInt(students.size()) ;
-//            int id =students.get(number).getId();
-            boolean b = studentService.deleteById(2);
-            //没有这个人或者kill失败 全局回滚
-            if(!b){
-                throw  new RuntimeException();
-            }
+//    @GlobalTransactional
+//    public String galobTrans(int age,String name){
+//            //db2+1 简单期间2班新来一个同学
+//            Student  student = new Student();
+//            student.setAge(age);
+//            student.setName(name);
+//            student.setCode(UUID.randomUUID().toString());
+//            Student student1 = studentService.insert2(student);
+//            //db1- 1班随机kill一个小朋友
+////            List<Student> students = studentService.queryAll();
+////            int number = new Random().nextInt(students.size()) ;
+////            int id =students.get(number).getId();
+//            boolean b = studentService.deleteById(2);
+//            //没有这个人或者kill失败 全局回滚
+//            if(!b){
+//                throw  new RuntimeException();
+//            }
+//
+//        return "success";
+//    }
 
-        return "success";
-    }
-
-    @GetMapping("/addStu")
-    public String addStu(int age,String name) {
-        return  this.galobTrans(age, name);
-    }
+//    @GetMapping("/addStu")
+//    public String addStu(int age,String name) {
+//        return  this.galobTrans(age, name);
+//    }
 }
